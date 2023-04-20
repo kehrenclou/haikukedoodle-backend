201396a5553a4c80e5df0fcc815f37b565a43811
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-04-20 13:43:14 -0700

    set up: install mongodb

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //backend/app.js
 /* --------------------------------- imports -------------------------------- */
 const express = require("express");
+const mongoose = require('mongoose');
 
 /* -------------------------- declare app and port -------------------------- */
 const app = express();
 const { PORT = 3001 } = process.env;
+
+// mongoose.connect("mongodb://localhost:27017/hkkd_db");//older node versions
+mongoose.connect('mongodb://127.0.0.1/hkkd_db');
 
 /* ----------------------------------- app ---------------------------------- */
 app.use(express.json()); //versions express >4.16 can use this instead of bodyparser
