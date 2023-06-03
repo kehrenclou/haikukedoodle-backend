87faac6cca5be49ac9bb11d9f42c4d2e42ce419b
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-06-03 13:50:06 -0700

    feat: add user model

---
+++ models/users.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+const UnauthorizedError = require("../errors/unauthorized");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "The name field must be filled in"],
+    minlength: [2, "The minimum length of the name field is 2"],
+    maxlength: [30, "The maximum length of the name field is 3"],
+  },
+  email: {
+    type: String,
+    required: [true, "The email field must be filled in"],
+    unique: [true, "The email is already registered"],
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: "Please enter a valid email.",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "The password field must be filled in"],
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email })
+    .select("password")
+    .orFail()
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(
+          new UnauthorizedError("Incorrect email or password")
+        );
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(
+            new UnauthorizedError("Incorrect email or password")
+          );
+        }
+        return user;
+      });
+    });
+};
+
+module.exports = mongoose.model("user", userSchema);
