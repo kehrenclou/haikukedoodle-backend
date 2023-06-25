93ea5263120bde7e9084aa0f349db93ca9e27017
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-06-25 09:48:46 -0700

    remove comments

--- routes/cards.js
+++ routes/cards.js
@@ -21,8 +21,8 @@ const {
   validateCardBody,
 } = require("../validations/validation");
 
-router.get("/", getCards);
-router.get("/:cardSkip", loadMoreCards);
+router.get("/", getCards);//avail to anonymous no auth
+router.get("/:cardSkip", loadMoreCards);//avail to anonymous no auth
 
 router.get("/:userId/bookmarks", auth, getBookmarks);
 router.get("/:cardSkip/:userId/bookmarks", auth, loadMoreBookmarks);
@@ -30,13 +30,15 @@ router.get("/:cardSkip/:userId/bookmarks", auth, loadMoreBookmarks);
 router.get("/:userId/cards", auth, getOwnerCards);
 router.get("/:cardSkip/:userId/cards", auth, loadMoreOwnerCards);
 
-router.post("/", validateCardBody, createCard);
+router.post("/", validateCardBody, createCard);//avail to anonymous no auth
 router.patch("/:cardId/owner", validateCardId, updateCardOwner);
 
-router.put("/:cardId/likes", validateCardId, likeCard);
-router.delete("/:cardId/likes", validateCardId, dislikeCard);
+router.put("/:cardId/likes", validateCardId, likeCard); //avail to anonymous no auth
+router.delete("/:cardId/likes", validateCardId, dislikeCard);//avail to anonymous no auth
+
 router.put("/:cardId/bookmarks", validateCardId, addBookmark);
 router.delete("/:cardId/bookmarks", validateCardId, removeBookmark);
+
 router.delete("/:cardId/delete", auth, validateCardId, deleteCard);
 
 module.exports = router;
