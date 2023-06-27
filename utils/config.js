f052f707a208de1ec7f29eba34288dab535b364d
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-06-27 12:10:39 -0700

    fix openaiKey

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,8 @@
 const { NODE_ENV, JWT_SECRET } = process.env; // secret saved on server in .env file
 const jwtSecret = NODE_ENV === "production" ? JWT_SECRET : "1234";
-const openaiKey =
-  NODE_ENV === "production"
-    ? REACT_APP_OPENAI_API_KEY
-    : process.env.REACT_APP_OPENAI_API_KEY;
+const openaiKey = process.env.REACT_APP_OPENAI_API_KEY;
+// NODE_ENV === "production"
+//   ? REACT_APP_OPENAI_API_KEY
+//   : process.env.REACT_APP_OPENAI_API_KEY;
 
 module.exports = { jwtSecret, openaiKey };
