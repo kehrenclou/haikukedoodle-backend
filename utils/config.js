7698d3841fadcd10fdcd715fc72d07214c12e847
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-06-27 12:19:24 -0700

    rename openai api key

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 const { NODE_ENV, JWT_SECRET } = process.env; // secret saved on server in .env file
 const jwtSecret = NODE_ENV === "production" ? JWT_SECRET : "1234";
-const openaiKey = process.env.REACT_APP_OPENAI_API_KEY;
+const openaiKey = process.env.OPENAI_API_KEY;
 // NODE_ENV === "production"
 //   ? REACT_APP_OPENAI_API_KEY
 //   : process.env.REACT_APP_OPENAI_API_KEY;
