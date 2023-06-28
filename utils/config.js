610e6acd7811db705a46bc869635cd0d1bfa962f
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-06-27 19:03:51 -0700

    fix lint

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 const { NODE_ENV, JWT_SECRET } = process.env; // secret saved on server in .env file
-const jwtSecret = NODE_ENV === "production" ? JWT_SECRET : "1234";
+const jwtSecret = NODE_ENV === 'production' ? JWT_SECRET : '1234';
 const openaiKey = process.env.OPENAI_API_KEY;
 // NODE_ENV === "production"
 //   ? REACT_APP_OPENAI_API_KEY
