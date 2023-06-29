ca30e132183ea09c76459fd2ed4e6eeb3a297869
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-06-29 14:03:58 -0700

    remove comments

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 const { NODE_ENV, JWT_SECRET, DB_ADDRESS } = process.env; // secret saved on server in .env file
 const jwtSecret = NODE_ENV === 'production' ? JWT_SECRET : '1234';
-// add the db address
+
 const openaiKey = process.env.OPENAI_API_KEY;
 const dbAddress = NODE_ENV === 'production' ? DB_ADDRESS : 'mongodb://127.0.0.1/hkkd_db';
-//add address to env file
+
 module.exports = { jwtSecret, openaiKey, dbAddress };
